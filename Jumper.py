a6d2bd855c461c07a911d4795f5bca0989c7704c
Romi27 <97213039+Romi27@users.noreply.github.com>
2022-06-07 15:58:52 -0300

    Update Jumper.py

--- Jumper.py
+++ Jumper.py
@@ -1,65 +1,62 @@
+from word_generator import Word_Generator
+
+
 class Jumper:
     def __init__(self):
         """ Creating the library and list to be printed later. This allows the lines of the library to be adjusted
         by adding the blank text "" later.
         """
-        #self.guess = "Y"
+       
         self.wrong_letter = 0
-        self.jumper_library = {
-        "1": ["  ___  "],
-        "2": [" /___\ "],
-        "3": [" \   / "],
-        "4": ["  \ /  "]}
+        self.jumper_library_1 = "  ___  "
+        self.jumper_library_2 = " /___\ "
+        self.jumper_library_3 = " \   / "
+        self.jumper_library_4 = "  \ /  "
+        self.jumper_library_5="0"
+        self.jumper_library_5_error="X"
         self.man_list = [
-        "  /|\  ",
-        "  / \  ",
-        "",
-        "^^^^^^^"]
-    def _falling_man(self):
+            "  /|\  ",
+            "  / \  ",
+            "",
+            "^^^^^^^"]
+
+    def get_drawing(self):
         """Using the Library and List to print image if guess is correct
         """
-        while self.guess.capitalize() == True:
-            a = self.jumper_library["1"]
-            b = self.jumper_library["2"]
-            c = self.jumper_library["3"]
-            d = self.jumper_library["4"]
-            print (a[0])
-            print (b[0])
-            print (c[0])
-            print (d[0])
-            print("   0   ")
-            for line in self.man_list:
-                print(line)
-                print()
-            """Adjusting the image for each missed guess"""
-        if not self.guess.capitalize() == True:
-            self.wrong_letter += 1
-            z = str(self.wrong_letter)
-            wrong_guess = self.jumper_library[z]
-            w = wrong_guess[0]
-            w = ""
-            empty_line = [w]
-            self.jumper_library[z] = empty_line
-            a = self.jumper_library["1"]
-            b = self.jumper_library["2"]
-            c = self.jumper_library["3"]
-            d = self.jumper_library["4"]
-            if self.attempt != 5:
-                print (a[0])
-                print (b[0])
-                print (c[0])
-                print (d[0])
-                print("   0   ")
-                for line in self.man_list:
-                    print(line)
-                    print()
-            else:
-                print (a[0])
-                print (b[0])
-                print (c[0])
-                print (d[0])
-                print("   X   ")
-                for line in self.man_list:
-                    print(line)
-                    print()
-                self.IsPlaying = False
+        draw = self.jumper_library_1
+        draw = self.jumper_library_2
+        draw=self.jumper_library_3
+        draw = self.jumper_library_4
+        draw=self.jumper_library_5
+        draw = self.man_list
+
+        if self.wrong_letter== 1:
+            draw = self.jumper_library_2
+            draw = self.jumper_library_3
+            draw = self.jumper_library_4
+            draw = self.jumper_library_5
+            draw = self.man_list
+        if self.wrong_letter == 2:
+            draw = self.jumper_library_3
+            draw = self.jumper_library_4
+            draw = self.jumper_library_5
+            draw = self.man_list
+        if self.wrong_letter == 3:
+            
+            draw = self.jumper_library_4
+            draw = self.jumper_library_5
+            draw = self.man_list
+        if self.wrong_letter == 4:
+          
+            draw = self.jumper_library_5
+            draw = self.man_list
+        if self.wrong_letter == 5:
+         
+            draw = self.jumper_library_5_error
+            draw = self.man_list
+            return draw
+
+    def wrongletter(self):
+        if self.wrong_letter not in Word_Generator.get_word:
+            self.wrong_letter=self.wrong_letter+1
+        return self.wrong_letter
