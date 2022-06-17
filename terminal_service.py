5d0d3e2fba6058c4f1ed71b70fa3ddddb60be16a
Hatsumi07 <64712742+Hatsumi07@users.noreply.github.com>
2022-06-17 00:12:13 -0500

    Update terminal_service.py

--- terminal_service.py
+++ terminal_service.py
@@ -1,3 +1,4 @@
+from word_generator import Word_Generator
 class Terminal:
     
     """Class Terminal will receive the word generated on the class Word and show the number of underscores needed to guess the word. Then it will ask for the user for an input.
@@ -8,54 +9,68 @@ class Terminal:
     The responsibility of a TerminalService is to provide input and output operations for the 
     terminal.
     """
-    def get_word (self, random_list):
-        '''It will get the word created on class Word and change every letter for underscores, creating a new list with underscores.
+    def __init__(self):
+        self._hidden_word = ""
+        self._hidden_word_list = []
+        self._word = Word_Generator()
+        self._guessed_letter = ""
+        self._check_guess = True
+        self._check_win = False
+        self._full_word = ""
+    def set_word(self, word):
+        self._word = word
+        self._create_hidden_word_list()
+        self._create_hidden_word()
+    def set_guessed_letter(self, guessed_letter):
+        self._guessed_letter = guessed_letter
+        self._add_guessed_letter()
+        word = self._word.get_word()
+        if self._full_word == word:
+            self._check_win = True
         
-        Arguments: 
+
+    def _add_guessed_letter(self):
+        word_list = self._word.get_word_as_list()
+        index = -1
+        check_letter = ""
+        for letter in word_list:
+            index += 1
+            if self._guessed_letter == letter:
+                self._hidden_word_list[index] = self._guessed_letter + " "
+                check_letter = self._guessed_letter
+        self._create_hidden_word()
+        if check_letter == self._guessed_letter:
+            self._check_guess = True
+        elif check_letter != self._guessed_letter:
+            self._check_guess = False
+        full_word = ""
+        for i in self._hidden_word.split():
+            full_word += i
+        self._full_word = full_word
+
+
+    def _create_hidden_word_list (self):
+        '''It will get the word created on class Word and change every letter for underscores, creating a new list with underscores.
+
+        Arguments:
         self: An instance of terminal service.
         list: A single  word in a list.
-
         Returns: a list with underscores.
         '''
-        num_letters = len(random_list)
-        editable_list =[]
-        for letter in random_list:
+        word_list = self._word.get_word_as_list()
+        for letter in word_list:
             letter = '_ '
-            editable_list.append(letter)
-        print(letter,end='')
-
-    def read_guess(self, guess):
-        """Gets the text input from the terminal.  
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            guess (string): A string of one letter. 
-
-        Returns:
-            string: The user's input as text.
-        """
-        return input(guess)
-
-    def compare_lists(self,editable_list, random_list, guess):
-        """Compares if the user's guess is included in the random_list (random word generated). 
-        If it is included it will print the new underscored word with the user's guess.
-        If not, it will tell the director that it was a wrong guess. 
-        
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            guess (string): The user's input. 
-            random_list (list with the random word): generated in the class Word
-            editable_list (list with underscores ) : Hiding the letters of the random list.
-                    Returns:
-            editable_list (list with underscores and user's  positive guess)
-        """
-                 
-
-        for i in range(len(random_list)):
-            if random_list[i] == guess:
-                editable_list[i] = guess
-        print (*editable_list)
-        return editable_list
-            
-        
-    
+            self._hidden_word_list.append(letter)
+    def _create_hidden_word(self):
+        hidden_word = ""
+        for letter in self._hidden_word_list:
+            hidden_word += letter
+        self._hidden_word = hidden_word
+    def get_guess(self):
+        return self._check_guess
+    def get_hidden_word(self):
+        return self._hidden_word
+    def get_hidden_word_list(self):
+        return self._hidden_word_list
+    def get_condition(self):
+        return self._check_win
