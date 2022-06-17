b9c6ba2cbf833b6d94ef1d9934fab358cf85d108
Hatsumi07 <64712742+Hatsumi07@users.noreply.github.com>
2022-06-17 00:07:57 -0500

    Update Jumper.py

--- Jumper.py
+++ Jumper.py
@@ -1,62 +1,43 @@
-from word_generator import Word_Generator
-
-
 class Jumper:
     def __init__(self):
         """ Creating the library and list to be printed later. This allows the lines of the library to be adjusted
         by adding the blank text "" later.
         """
-       
-        self.wrong_letter = 0
-        self.jumper_library_1 = "  ___  "
-        self.jumper_library_2 = " /___\ "
-        self.jumper_library_3 = " \   / "
-        self.jumper_library_4 = "  \ /  "
-        self.jumper_library_5="0"
-        self.jumper_library_5_error="X"
-        self.man_list = [
-            "  /|\  ",
-            "  / \  ",
-            "",
-            "^^^^^^^"]
+        self._parachute = ["  ___  \n", " /___\ \n", " \   / \n", "  \ /  \n"]
+        self._jumper = ["   0  \n", "  /|\  \n", "  / \  \n", "^^^^^^^"]
+        self._guess_is_right = True
+        self._is_alive = True
 
-    def get_drawing(self):
+    def set_guess(self, guess):
         """Using the Library and List to print image if guess is correct
         """
-        draw = self.jumper_library_1
-        draw = self.jumper_library_2
-        draw=self.jumper_library_3
-        draw = self.jumper_library_4
-        draw=self.jumper_library_5
-        draw = self.man_list
+        self._guess_is_right = guess
+        self._check_guess()
 
-        if self.wrong_letter== 1:
-            draw = self.jumper_library_2
-            draw = self.jumper_library_3
-            draw = self.jumper_library_4
-            draw = self.jumper_library_5
-            draw = self.man_list
-        if self.wrong_letter == 2:
-            draw = self.jumper_library_3
-            draw = self.jumper_library_4
-            draw = self.jumper_library_5
-            draw = self.man_list
-        if self.wrong_letter == 3:
-            
-            draw = self.jumper_library_4
-            draw = self.jumper_library_5
-            draw = self.man_list
-        if self.wrong_letter == 4:
-          
-            draw = self.jumper_library_5
-            draw = self.man_list
-        if self.wrong_letter == 5:
-         
-            draw = self.jumper_library_5_error
-            draw = self.man_list
-            return draw
+    def _check_guess(self):
+        if len(self._parachute) > 1:
+            if self._guess_is_right == False:
+                self._parachute.pop(0)
+        elif len(self._parachute) == 1:
+            if self._guess_is_right == False:
+                self._parachute.pop(0)
+                self._jumper[0] = "   x  \n"
+                self._is_alive = False
 
-    def wrongletter(self):
-        if self.wrong_letter not in Word_Generator.get_word:
-            self.wrong_letter=self.wrong_letter+1
-        return self.wrong_letter
+    def _draw_parachute(self):
+        parachute = ""
+        for line in self._parachute:
+            parachute += line
+        return parachute
+    def _draw_jumper(self):
+        parachute = self.get_parachute()
+        jumper = parachute 
+        for line in self._jumper:
+            jumper += line
+        return jumper 
+    def get_parachute(self):
+        return self._draw_parachute()
+    def get_jumper(self):
+        return self._draw_jumper()
+    def get_condition(self):
+        return self._is_alive
